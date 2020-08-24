38e5fa1a76eaa7630b5b307e4e020f290aa25254
heyeway <heyewayxysxd@gmail.com>
2020-08-23 21:33:25 -0400

    fix png2html.sh

--- png2html.sh
+++ png2html.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "<html><body><br>" > web.html
 
-ls *.png | awk -F ":" '{print $1":\n<br><img src=\""$1""$2"\" width=600><br>"}' >> web.html
+ls *.png | awk -F ":" '{print $1":\n<br><img src=\""$1"\" width=600><br>"}' >> web.html
 
 echo "</body></html>" >> web.html
